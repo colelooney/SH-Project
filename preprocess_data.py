6dfdc817c8646a5742ab1a7a943180f2d7b8d0b1
Cole Looney <cole@thymia.ai>
2025-10-08 17:14:58 +0100

    update to save Lumi_weight for plotting

--- preprocess_data.py
+++ preprocess_data.py
@@ -19,14 +19,15 @@ with h5py.File(relative_path) as f:
     df = pd.DataFrame(f['LargeRJet']['1d'][:])
 
 X = df.drop(columns=['Lumi_weight'])
-y = df['Lumi_weight']
+y = df['Lumi_weight'].copy()
+lumi_weights =  df['Lumi_weight'].copy()
 
 y[y>0] = 1
 y[y<0] = 0
 
 y = np.array(y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test, lumi_train, lumi_test = train_test_split(X, y,lumi_weights, test_size=0.2, random_state=42, stratify = y)
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
