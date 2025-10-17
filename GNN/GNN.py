23666fcfaf264cb1a003ce75d7a54fcaf4fa33cc
Cole Looney <colelooney22@gmail.com>
2025-10-17 12:32:43 +0100

    update model arch

--- GNN/GNN.py
+++ GNN/GNN.py
@@ -1,20 +1,34 @@
-from torch_geometric.nn import GCNConv
-from torch.nn.functional import F
+from torch_geometric.nn import GCNConv, global_mean_pool
+import torch.nn.functional as F
 import torch
+import torch.nn as nn
 
 class GCN(torch.nn.Module):
     def __init__(self, input_size, hidden_dim):
         super().__init__()
         torch.manual_seed(12345)
         self.conv1 = GCNConv(input_size, hidden_dim)
-        self.conv2 = GCNConv(hidden_dim, 1)
+        self.bn1 = nn.BatchNorm1d(hidden_dim)
+        self.relu = nn.ReLU()
+        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.bn2 = nn.BatchNorm1d(hidden_dim)
+        self.classifier = nn.Linear(hidden_dim, 1)
 
-    def forward(self, x, edge_index):
-        x = self.conv1(x,edge_index)
-        x = x.relu()
-        x = F.dropout(x, p =0.5,training = self.training)
+
+    def forward(self, x, edge_index, batch):
+        x = self.conv1(x, edge_index)
+        x = self.bn1(x)
+        x = self.relu(x)
+
         x = self.conv2(x, edge_index)
-        return torch.sigmoid(x) #return probabilities for binary classification
+        x = self.bn2(x)
+        x = self.relu(x)
+        
+        x= global_mean_pool(x, batch)
+        x = self.classifier(x)
+        x = torch.sigmoid(x)
+
+        return x #return probabilities for binary classification
 
 if __name__ == "__main__":
     print("This file only contains the GCN model definition.")
