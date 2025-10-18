ffbea8580a29c16a8998b4dbd8be7472708f6037
Cole Looney <colelooney22@gmail.com>
2025-10-18 15:56:18 +0100

    update architecture

--- GNN/GNN.py
+++ GNN/GNN.py
@@ -1,4 +1,4 @@
-from torch_geometric.nn import GCNConv, global_mean_pool
+from torch_geometric.nn import GCNConv, global_mean_pool, BatchNorm
 import torch.nn.functional as F
 import torch
 import torch.nn as nn
@@ -12,18 +12,29 @@ class GCN(torch.nn.Module):
         self.relu = nn.ReLU()
         self.conv2 = GCNConv(hidden_dim, hidden_dim)
         self.bn2 = nn.BatchNorm1d(hidden_dim)
+
+        self.conv3 = GCNConv(hidden_dim, hidden_dim)
+        self.bn3 = nn.BatchNorm1d(hidden_dim)
+
+        self.conv4 = GCNConv(hidden_dim, hidden_dim)
+        self.bn4 = nn.BatchNorm1d(hidden_dim)
         self.classifier = nn.Linear(hidden_dim, 1)
+
 
 
     def forward(self, x, edge_index, batch):
         x = self.conv1(x, edge_index)
-        x = self.bn1(x)
         x = self.relu(x)
+        x = self.bn1(x)
 
         x = self.conv2(x, edge_index)
-        x = self.bn2(x)
         x = self.relu(x)
-        
+        x = self.bn2(x)
+
+        x = self.conv3(x, edge_index)
+        x = self.relu(x)
+        x = self.bn3(x)
+
         x= global_mean_pool(x, batch)
         x = self.classifier(x)
         x = torch.sigmoid(x)
@@ -33,5 +44,5 @@ class GCN(torch.nn.Module):
 if __name__ == "__main__":
     print("This file only contains the GCN model definition.")
     print("Please run the GNN_SignalvsBackground.py script to train and evaluate the model.")
-    model = GCN(input_size = 10, hidden_dim = 64)
-    print("example model initisation: ", + model)
+    # model = GCN(input_size = 10, hidden_dim = 64)
+    # print("example model initisation: ", + model)
