388cd0f5286634e20a0ddd12e27d57733d625371
Cole Looney <colelooney22@gmail.com>
2025-10-25 12:35:25 +0100

    add early stopper class

--- DNN/DNN_models.py
+++ DNN/DNN_models.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 from preprocess_large_data import X_train_tensor
 hidden_dim = 64
 output_dim = 1
-input_size = X_train_tensor.shape[0]
 
 class DNN(nn.Module):
     def __init__(self, input_size):
@@ -20,3 +19,20 @@ class DNN(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
+    
+class EarlyStopper:
+    def __init__(self,patience = 1, min_delta = 0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
